1c17ac9ac1c0db320e9049b5d310543579dc10e0
motoglucas2024-ds <motoglucas2024@gmail.com>
2026-08-12 14:08:56 -0300

    Add files via upload

--- deathsWatcher.js
+++ deathsWatcher.js
@@ -16,10 +16,22 @@ async function checkLatestDeaths(client) {
     // Petición a la sección de muertes de Eldrian en Rubinot
     const url = 'https://rubinot.com.br/deaths?world=Eldrian'; 
     const { data: html } = await axios.get(url, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-      }
-    });
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'es-ES,es;q=0.9,en;q=0.8',
+    'Cache-Control': 'no-cache',
+    'Pragma': 'no-cache',
+    'Sec-Ch-Ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"Windows"',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1'
+  }
+});
 
     const $ = cheerio.load(html);
     const newDeaths = [];
